db3dfd988df1956edf7be6589f5e9373ecca93e3
Geoff Flarity <geoff@well.ca>
2012-02-06 15:36:06 -0500

    added package.json, missed bin/nervous and plugins/test.js last commit

--- plugins/test.js
+++ plugins/test.js
@@ -3,4 +3,20 @@ var config = {};
 
 var axon_factory = require('../lib/axon_factory')( system_type, config );
 
-require( './' + process.argv[2] )( axon_factory(  process.argv[2] ) );
+var usage =  'Usage: node test.js <plugin_name>';
+
+if ( process.argv[2] ) {
+    
+    try {
+	require(  process.argv[2] )( axon_factory(  process.argv[2] ) );
+    } catch ( e ) {
+
+	console.log( usage );
+    }
+}
+else {
+
+    console.log( usage );
+}
+
+
