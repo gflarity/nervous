954d3dd78736a27818019c6c869ab38a9503db6e
Geoff Flarity <geoff@well.ca>
2012-02-08 16:18:20 -0500

    abandoned install command as there's a different version used on OSX, Linux, SmartOS.... GRR

--- install.sh
+++ install.sh
@@ -1,15 +1,4 @@
 set -e
-
-UNAME=`uname`;
-INSTALL='install'
-if [ $UNAME = 'Darwin' ]; then
-    if which -s ginstall; then 
-	INSTALL='ginstall'
-    else 
-	echo "Hi Mac user, please install coreutils via brew"
-    fi
-fi
-
 DEFAULT_PREFIX=/opt
 PREFIX=${PREFIX:-$DEFAULT_PREFIX}
 
@@ -18,8 +7,8 @@ PREFIX=${PREFIX:-$DEFAULT_PREFIX}
 /bin/mkdir -p $PREFIX/nervous/lib/node_modules
 /bin/mkdir -p $PREFIX/nervous/conf
 /bin/mkdir -p $PREFIX/nervous/plugins/node_modules
-$INSTALL -m 0755 -t $PREFIX/nervous/bin bin/nervous	
-$INSTALL -m 0644 -t $PREFIX/nervous/plugins plugins/test.js
-$INSTALL -m 0644 conf/nervous.json $PREFIX/nervous/conf/nervous.json.example
-$INSTALL -m 0644 -t $PREFIX/nervous/lib lib/*js
+cp bin/nervous $PREFIX/nervous/bin/
+cp plugins/test.js $PREFIX/nervous/plugins/
+cp conf/nervous.json $PREFIX/nervous/conf/nervous.json.example
+cp lib/*js $PREFIX/nervous/lib/
 cp -r lib/node_modules/* $PREFIX/nervous/lib/node_modules/
