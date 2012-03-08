18e3fc12721652a8f7096ff3a5c65c9483c54f0f
Geoff Flarity <geoff@well.ca>
2012-03-08 13:20:33 -0500

    nervous now strips nervous_plugin_ from the sub namespace that gets sent to graphite, it's also no necessary for testing

--- plugins/test.js
+++ plugins/test.js
@@ -7,11 +7,23 @@ var usage =  'Usage: node test.js <plugin_name>';
 
 if ( process.argv[2] ) {
     
+    //strip the nervous_plugin from in front if its there
+    var subspace = process.argv[2].replace(/^nervous_plugin_/, '');
+    
     try {
-	require(  process.argv[2] )( axon_factory(  process.argv[2] ) );
+	    require( process.argv[2] )( axon_factory( subspace ) );
     } catch ( e ) {
 
-	console.log( usage );
+        //we also support dropping the nervous_plugin_ for testing:        
+        try { 
+            require( 'nervous_plugin_' + process.argv[2]  )( axon_factory( subspace ) );
+        }
+        catch ( e ) {
+            //finally, usage and exit code 1!
+            console.log( usage );
+            process.exit(1);
+        }
+        
     }
 }
 else {
